01c9e310cdd6ea4108869ebe02e900691a3c4044
Patrick Mvula <mvlapatrick@gmail.com>
2025-04-07 20:21:52 +0200

    feat: set_var func

--- animepahe-dwnld.sh
+++ animepahe-dwnld.sh
@@ -9,8 +9,24 @@ usage() {
 
 set_var() {
     _CURL="$(command -v curl)" || command_not_found "curl"
-    _JQ="$(command -v jp)" || command_not_found "jp"
+    _JP="$(command -v jp)" || command_not_found "jp"
     _FZF="$(command -v fzf)" || command_not_found "fzf"
-    if [[-z $(ANIMEPAHE_DL_NODE:-)]]; then
+    if [[ -z ${ANIMEPAHE_DL_NODE:-} ]]; then
+        _NODE="$(command -v node)" || command_not_found "node"
+    else
+        _NODE="$ANIMEPAHE_DL_NODE"
+    fi
+    _FFMPEG="$(command -v ffmpeg)" || command_not_found "ffmpeg"
+    if [[ ${_PARALLEL_JOBS:-} -gt 1 ]]; then
+        _OPENSSL="$(command -v openssl)" || command_not_found "openssl"
+    fi
 
+    _HOST="https://animepahe.ru"
+    _ANIME_URL="$_HOST/anime"
+    _API_URL="$_HOST/api"
+    _REFERER_URL="$_HOST"
+
+    _SCRIPT_PATH=$(dirname "$(realpath "$0")")
+    _ANIME_LIST_FILE="$_SCRIPT_PATH/anime.list"
+    _SOURCE_FILE=".source.json"
 }
